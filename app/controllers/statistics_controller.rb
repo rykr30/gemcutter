6f4fc73a0eafc965c5f88e7f538526cc303a3e34
rick <technoweenie@gmail.com>
2009-10-11 23:38:15 -0700

    replace calls to gcharts with inlined images generated by rack-sparklines

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,9 +4,6 @@ class StatisticsController < ApplicationController
     @number_of_users          = User.count
     @number_of_downloads      = Rubygem.sum(:downloads)
     @most_downloaded          = Rubygem.downloaded(10)
-    @recent_gems_created      = Rubygem.sparkline_data_from_past(30.days)
-    @recent_users_created     = User.sparkline_data_from_past(30.days)
-    @recent_downloads_created = Download.sparkline_data_from_past(30.days)
   end
 
   def recent_gems_created
--- test/functional/statistics_controller_test.rb
+++ test/functional/statistics_controller_test.rb
@@ -7,17 +7,11 @@ class StatisticsControllerTest < ActionController::TestCase
       @number_of_users          = 101
       @number_of_downloads      = 42
       @most_downloaded          = [Factory(:rubygem)]
-      @recent_gems_created      = [10, 0]
-      @recent_users_created     = [20, 0]
-      @recent_downloads_created = [30, 0]
 
       stub(User).count { @number_of_users }
-      stub(User).sparkline_data_from_past { @recent_users_created }
       stub(Rubygem).total_count { @number_of_gems }
       stub(Rubygem).sum { @number_of_downloads }
       stub(Rubygem).downloaded { @most_downloaded }
-      stub(Rubygem).sparkline_data_from_past { @recent_gems_created }
-      stub(Download).sparkline_data_from_past { @recent_downloads_created }
 
       get :index
     end
@@ -28,9 +22,6 @@ class StatisticsControllerTest < ActionController::TestCase
     should_assign_to(:number_of_users) { @number_of_users }
     should_assign_to(:number_of_downloads) { @number_of_downloads }
     should_assign_to(:most_downloaded) { @most_downloaded }
-    should_assign_to(:recent_gems_created) { @recent_gems_created }
-    should_assign_to(:recent_users_created) { @recent_users_created }
-    should_assign_to(:recent_downloads_created) { @recent_downloads_created }
 
     should "display number of gems" do
       assert_contain "1,337"
@@ -46,12 +37,9 @@ class StatisticsControllerTest < ActionController::TestCase
 
     should "load up the number of gems, users, and downloads" do
       assert_received(User)     { |subject| subject.count }
-      assert_received(User)     { |subject| subject.sparkline_data_from_past.with(30.days) }
       assert_received(Rubygem)  { |subject| subject.total_count }
       assert_received(Rubygem)  { |subject| subject.sum.with(:downloads) }
       assert_received(Rubygem)  { |subject| subject.downloaded.with(10) }
-      assert_received(Rubygem)  { |subject| subject.sparkline_data_from_past.with(30.days) }
-      assert_received(Download) { |subject| subject.sparkline_data_from_past.with(30.days) }
     end
   end
 
